0a3c53acd21f4ef50852a5791ab1cc0a3e9f143d
{ Walyson } <walysonmoura222@gmail.com>
2024-06-25 00:32:17 +0000

    feat: add initial setup and integration with lint and test tools
    What was done:

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint', 'prettier --write'],
+  //'*.{ts,tsx}': ['tsc --noEmit'],
+  '**/*.ts?(x)': () => 'pnpm test',
+  '*.{json,yaml}': ['prettier --write'],
+}
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import swc from 'unplugin-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+})

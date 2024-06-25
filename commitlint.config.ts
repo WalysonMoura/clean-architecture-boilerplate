922732a240abb5cd86d18d413d18aa5f2006b530
{ Walyson } <walysonmoura222@gmail.com>
2024-06-25 01:02:26 +0000

    fix(dependabot): correct package-ecosystem configuration to npm
    - Fixes the value of the `package-ecosystem` field in the `dependabot.yml` file to `npm`
    - Ensures Dependabot is correctly configured to check for npm package updates
    - Adjusts the YAML file structure to avoid syntax errors
    
    This configuration sets the package ecosystem to npm, specifies the root directory for checks, configures weekly update checks, sets a prefix for commit messages, limits the number of open pull requests, and sets the rebase strategy to auto.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 1120],
+  },
 }
 
 export default Configuration

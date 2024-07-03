7fbeb3f50ce0465cc56c9e617beff6c9228cc253
{ Walyson } <walysonmoura222@gmail.com>
2024-07-03 19:21:20 +0000

    fix: lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint', 'prettier --write'],
   '**/*.ts?(x)': () => 'npm run check-types',
-  '**/*.spec.ts': ['vitest run', 'vitest run --config ./vitest.config.e2e.ts'],
+  '**/*.spec.ts': ['vitest run'],
   '*.{json,yaml}': ['prettier --write'],
 }

c16f6733312975d2fb29173f4127773d4896468e
{ Walyson } <walysonmoura222@gmail.com>
2024-07-09 12:36:12 +0000

    feat: add swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { apiReference } from '@scalar/nestjs-api-reference'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  app.use(
+    '/reference',
+
+    apiReference({
+      cdn: 'https://cdn.jsdelivr.net/npm/@scalar/api-reference',
+      spec: {
+        content: document,
+      },
+    })
+  )
+
   await app.listen(3000)
 }
 bootstrap()
